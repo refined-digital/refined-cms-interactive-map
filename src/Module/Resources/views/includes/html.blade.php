254b49c5de28ac0b5fe2a5aafb550dd9baa7d3ac
Matthias Law <matthias@refineddigital.co.nz>
2022-08-31 20:05:22 +1200

    feat: enabling categories

--- src/Module/Resources/views/includes/html.blade.php
+++ src/Module/Resources/views/includes/html.blade.php
@@ -1,32 +1,51 @@
-<div class="map">
-  <div class="holder">
-    <div class="map__left">
-      <div id="map__holder"></div>
-    </div>
-    <div class="map__right">
-      @if (isset($mapSettings->heading) && $mapSettings->heading->value)
-        <h3 class="heading">{{ $mapSettings->heading->value }}</h3>
-      @endif
-      @if (isset($mapSettings->content) && $mapSettings->content->value)
-        <div class="map__content">{!! $mapSettings->content->value !!}</div>
-      @endif
+<div class="map flex">
+  <div class="map__left fade-in-up">
+    <div id="map__holder"></div>
+  </div>
+  <div class="map__right fade-in-up">
+    @if (isset($mapSettings->heading) && $mapSettings->heading->value)
+      <h3 class="heading">{{ $mapSettings->heading->value }}</h3>
+    @endif
+    @if (isset($mapSettings->content) && $mapSettings->content->value)
+      <div class="map__content">{!! $mapSettings->content->value !!}</div>
+    @endif
+    @if($categories->count())
+      <nav class="map__categories">
+        <ul>
+          @php $index = 0; @endphp
+          @foreach ($categories as $cat)
+            @if ($cat->markers->count())
+              <li class="map__category-item">
+              <span class="map__category-text title" data-id="{{ $cat->id }}">
+                {{ $cat->name }}
+              </span>
+                <ul>
+                  @foreach ($cat->markers as $marker)
+                    @php $index ++ @endphp
+                    <li class="map__marker-item" data-id="{{ $marker->id }}">
+                    <span class="map__category-number">
+                      {{ $index }}
+                    </span>
+                      <span class="map__marker-text">
+                      {{ $marker->name }}
+                    </span>
+                    </li>
+                  @endforeach
+                </ul>
+              </li>
+            @endif
+          @endforeach
+        </ul>
+      </nav>
+    @endif
 
-      @if($categories->count())
-        <nav class="map__categories">
-          <ul>
-            @foreach ($categories as $cat)
-              <li class="map__category-item" data-id="{{ $cat->id }}">{{ $cat->name }}</li>
-            @endforeach
-          </ul>
-        </nav>
-      @endif
-
-      @if (isset($mapSettings->map_link) && $mapSettings->map_link->value)
-      <footer class="content-block__footer">
-        <a href="{{ $mapSettings->map_link->value }}" class="button" target="_blank">Open in Google Maps</a>
+    @if (isset($mapSettings->map_link) && $mapSettings->map_link->value)
+      <footer class="content-block__link">
+        <a href="{{ $mapSettings->map_link->value }}" class="link" target="_blank">
+          Open in Google Maps
+        </a>
       </footer>
-      @endif
+    @endif
 
-    </div>
   </div>
 </div>
--- src/Module/Resources/views/map.blade.php
+++ src/Module/Resources/views/map.blade.php
@@ -3,14 +3,26 @@
   $mapSettings = settings()->get('interactive-map');
   $lat = isset($mapSettings->default_latitude) && $mapSettings->default_latitude->value ? $mapSettings->default_latitude->value : -34.8096626;
   $lng = isset($mapSettings->default_longitude) && $mapSettings->default_longitude->value ? $mapSettings->default_longitude->value : 138.6369941;
-  $categorySelector = '.map__category-item';
+  $categorySelector = '.map__category-text';
   $markerSelector = '.map__marker-item';
+
+  if ($categories->count()) {
+      $c = 0;
+      foreach ($categories as $category) {
+          if ($category->markers->count()) {
+              foreach ($category->markers as $marker) {
+                  $c ++;
+                  $text = (string)$c;
+                  $label = new stdClass();
+                  $label->className = 'map__marker-label';
+                  $marker->label = $label;
+              }
+          }
+      }
+  }
 @endphp
 
 @include('interactive-map::includes.html')
 
-@include('interactive-map::includes.styles')
-
 @include('interactive-map::includes.scripts')
-
 
